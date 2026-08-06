507734fbe9c266e586341a4680be3fbe3c6957e7
Tony91590 <91764010+Tony91590@users.noreply.github.com>
2026-08-07 01:54:37 +0200

    Remove original config file for mt7981_ax3000t

--- diy-part.sh
+++ diy-part.sh
@@ -7,3 +7,4 @@ rm -f uboot-mtk-20220606/cmd/glbtn.c.orig
 rm -f uboot-mtk-20220606/arch/arm/dts/mt7981-ax3000t.dts.orig
 rm -f uboot-mtk-20230718-09eda825/cmd/glbtn.c.orig
 rm -f uboot-mtk-20230718-09eda825/arch/arm/dts/mt7981-ax3000t.dts.orig
+rm -f atf-20220606-637ba581b/configs/mt7981_ax3000t_defconfig.orig
